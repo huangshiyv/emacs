727cf5380a530bd42e77edce68fff04d86962b53
HUANG <I067643@global.corp.sap>
2016-10-11 14:08:29 +0200

    change binding key for magit

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -15,4 +15,7 @@
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
 (global-set-key (kbd "C-c p f") 'counsel-git)
+
+(global-set-key (kbd "C-c g") 'magit-status)
+
 (provide 'init-keybindings)
